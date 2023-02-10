259b39827db1f8e6b53d282f0fccdb0d5a65f4a6
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-10 11:40:51 +0530

    Refactor: extracted methods for getting expense name and meal over expense marker

--- expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
+++ expensereport-kotlin/src/main/kotlin/com/nelkinda/training/ExpenseReport.kt
@@ -22,21 +22,21 @@ class ExpenseReport {
             if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST) {
                 mealExpenses += expense.amount
             }
-
-            val expenseName = when (expense.type) {
-                ExpenseType.DINNER -> "Dinner"
-                ExpenseType.BREAKFAST -> "Breakfast"
-                ExpenseType.CAR_RENTAL -> "Car Rental"
-            }
-
-            val mealOverExpensesMarker = if (expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000) "X" else " "
-
-            println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker)
+            println(getExpenseName(expense) + "\t" + expense.amount + "\t" + getMealOverExpensesMarker(expense))
 
             total += expense.amount
         }
 
         println("Meal expenses: $mealExpenses")
         println("Total expenses: $total")
+    }
+
+    private fun getMealOverExpensesMarker(expense: Expense) =
+        if (expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000) "X" else " "
+
+    private fun getExpenseName(expense: Expense) = when (expense.type) {
+        ExpenseType.DINNER -> "Dinner"
+        ExpenseType.BREAKFAST -> "Breakfast"
+        ExpenseType.CAR_RENTAL -> "Car Rental"
     }
 }
