a5428f66b972108f8654d2358bb8c62377f733be
Arun Murugan <arunmurugan@sahaj.ai>
2023-02-10 11:23:01 +0530

    Added unit test for print report method

---
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseReportTest.kt
@@ -0,0 +1,55 @@
+package com.nelkinda.training
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class ExpenseReportTest() {
+    private val standardOut: PrintStream = System.out;
+    private val outputStreamCaptor: ByteArrayOutputStream = ByteArrayOutputStream();
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    fun createExpense(type: ExpenseType, amount: Int): Expense {
+        val expense = Expense()
+        expense.type = type
+        expense.amount = amount
+
+        return expense
+    }
+
+    @Test
+    fun shouldPrintReport() {
+        val expenses = listOf<Expense>(
+            createExpense(ExpenseType.DINNER, 1000),
+            createExpense(ExpenseType.BREAKFAST, 1000),
+            createExpense(ExpenseType.CAR_RENTAL, 1000),
+            createExpense(ExpenseType.DINNER, 6000),
+            createExpense(ExpenseType.BREAKFAST, 2000),
+        )
+
+        ExpenseReport().printReport(expenses)
+
+        val expectedResult = "Dinner\t1000\t \n" +
+                "Breakfast\t1000\t \n" +
+                "Car Rental\t1000\t \n" +
+                "Dinner\t6000\tX\n" +
+                "Breakfast\t2000\tX\n" +
+                "Meal expenses: 10000\n" +
+                "Total expenses: 11000\n"
+        val actualOutput = outputStreamCaptor.toString()
+        val actualOutputList = actualOutput.split("\n")
+        assertEquals(expectedResult, actualOutputList.subList(1, actualOutputList.size).joinToString("\n"))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+}
